874f54d21bc95b34fbf9c612fae116531d8f99b5
lutzidan <lutzidan@students.zhaw.ch>
2023-01-07 10:53:28 +0100

    added drawing shape project

---
+++ src/lecture2/Main.java
@@ -0,0 +1,29 @@
+package lecture2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int marks[] = new int[6];
+        int i;
+        float total = 0, avg;
+        Scanner scanner = new Scanner(System.in);
+
+        for(i=0; i<6; i++){
+            System.out.println("Etner Marks of Subject " + (i+1));
+            marks[i] = scanner.nextInt();
+            total = total + marks[i];
+        }
+        scanner.close();
+
+        avg = total/6;
+        System.out.print("The studet grade is: ");
+        if (avg>=80){
+            System.out.println("A");
+        } else if (avg>=60 && avg < 80){
+            System.out.println("B");
+        } else {
+            System.out.println("C");
+        }
+    }
+}
